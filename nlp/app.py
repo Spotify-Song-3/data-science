30c3c15414eb0dba0a78996aa7c93d916c86ee07
Rob LeCheminant <roblecheminant@gmail.com>
2020-03-05 21:40:19 -0700

    updating models and app.py

--- nlp/app.py
+++ nlp/app.py
@@ -4,30 +4,35 @@ import requests
 import json
 import numpy as np
 
+from nlp_models import processed_score, stemmed_score, get_lemmas, lemma_score
+
 import nltk
 nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 # Model
 sid = SentimentIntensityAnalyzer()
+nlp = lemma_score()
 
 # Pickle it
-pickle.dump(sid, open('model.pkl', 'wb'))
+pickle.dump(nlp, open('model.pkl', 'wb'))
 model = pickle.load(open('model.pkl', 'rb'))
+
+
 
 # Create App
 app = Flask(__name__)
 
-# @app.route('/', methods = ['GET', 'POST'])
-# def analysis():
+@app.route('/', methods = ['GET', 'POST'])
+def analysis():
 
-#     # Take JSON input
-#     text = request.get_json(force = True)
-#     # Run JSON as text through model
-#     prediction = model.polarity_scores(str(text))
+    # Take JSON input
+    text = request.get_json(force = True)
+    # Run JSON as text through model
+    prediction = model.polarity_scores(str(text))
 
-#     # Re-convert results to JSON
-#     return jsonify(results = prediction)
+    # Re-convert results to JSON
+    return jsonify(results = prediction)
 
 if __name__ == '__main__':
     app.run()
---
+++ nlp/nlp_models.py
@@ -0,0 +1,94 @@
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+nltk.download('vader_lexicon')
+nltk.download('punkt')
+sid = SentimentIntensityAnalyzer()
+
+import spacy
+from spacy.tokenizer import Tokenizer
+import en_core_web_md
+nlp = en_core_web_md.load()
+tokenizer = Tokenizer(nlp.vocab)
+STOP_WORDS = nlp.Defaults.stop_words
+
+import pandas as pd 
+from nltk.stem import PorterStemmer
+ps = PorterStemmer()
+
+def processed_score(data=lyrics):
+    tokens = []
+    df = pd.DataFrame(data.split(), columns=['words'])
+
+    for doc in tokenizer.pipe(df['words']):
+        doc_tokens = []
+        for token in doc:
+            if (token.is_stop == False) & (token.is_punct == False):
+                doc_tokens.append(token.text.lower())
+        tokens.append(doc_tokens)
+
+    df['tokens'] = tokens
+    word_list = sum(list([item for item in df['tokens'] if len(item) != 0]), [])
+    lyrics_processed = ' '.join(word_list)
+    scores = sid.polarity_scores(lyrics_processed)
+
+    return scores
+
+def stemmed_score(data=lyrics):
+    """ Processes the text via spacy """
+    tokens = []
+    words = []
+
+    # Stemming
+    for word in data.split():
+        words.append(ps.stem(word))
+
+    df = pd.DataFrame(words, columns=['words'])
+
+    for doc in tokenizer.pipe(df['words']):
+        doc_tokens = []
+        for token in doc:
+            if (token.is_stop == False) & (token.is_punct == False):
+                doc_tokens.append(token.text.lower())
+        tokens.append(doc_tokens)
+
+    df['tokens'] = tokens
+    word_list = sum(list([item for item in df['tokens'] if len(item) != 0]), [])
+    lyrics_processed = ' '.join(word_list)
+    scores = sid.polarity_scores(lyrics_processed)
+
+    return scores
+
+def get_lemmas(data):
+    """ Gets lemmas for text """
+    lemmas = []
+    doc = nlp(data)
+    
+    for token in doc: 
+        if ((token.is_stop == False) and (token.is_punct == False)) and (token.pos_!= 'PRON'):
+            lemmas.append(token.lemma_)
+    
+    return lemmas
+
+def lemma_score(data=lyrics):
+    """ Processes the text via spacy """
+    tokens = []
+    words = []
+
+    # Lemmatization
+    words = get_lemmas(lyrics)
+
+    df = pd.DataFrame(words, columns=['words'])
+
+    for doc in tokenizer.pipe(df['words']):
+        doc_tokens = []
+        for token in doc:
+            if (token.is_stop == False) & (token.is_punct == False):
+                doc_tokens.append(token.text.lower())
+        tokens.append(doc_tokens)
+
+    df['tokens'] = tokens
+    word_list = sum(list([item for item in df['tokens'] if len(item) != 0]), [])
+    lyrics_processed = ' '.join(word_list)
+    scores = sid.polarity_scores(lyrics_processed)
+
+    return scores
